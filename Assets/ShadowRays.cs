c33a9215e80377488416dde56da5bc9601e80d72
Yacoub Ahmed <dragon.fly1212@yahoo.co.uk>
2016-06-17 23:46:03 +0100

    Working!
    Got the shadows working, found out the problem seemed to be not wrapping
    the last three vertices together to complete the mesh, as well as not
    converting the points from local to global space.

--- Assets/ShadowRays.cs
+++ Assets/ShadowRays.cs
@@ -6,55 +6,82 @@ public class ShadowRays : MonoBehaviour {
 
 	public float viewRadius;
 	public List<Vector2> allPoints, hitPoints;
-	public List<float> angles;
+	public float accuracyDegree, offset;
+	public MeshFilter mf;
+	Mesh shadowMesh;
 
 	void Start () {
 		allPoints = new List<Vector2> ();
+		mf = GetComponent<MeshFilter> ();
+		shadowMesh = new Mesh ();
+		shadowMesh.name = "Shadow Mesh";
+		mf.mesh = shadowMesh;
 	}
 
 	void Update () {
+		
 		PolygonCollider2D[] polys = GetColliders ();
 		allPoints.Clear ();
 		hitPoints.Clear ();
-		allPoints.Add ((Vector2)transform.position + Vector2.up * viewRadius);
-		allPoints.Add ((Vector2)transform.position + Vector2.right * viewRadius);
-		allPoints.Add ((Vector2)transform.position + Vector2.down * viewRadius);
-		allPoints.Add ((Vector2)transform.position + Vector2.left * viewRadius);
+		for (float i = 0; i <= (2) * Mathf.PI; i += accuracyDegree) {
+			allPoints.Add ((Vector2)transform.position + new Vector2 (Mathf.Sin (i), Mathf.Cos (i)));
+		}
 
 		foreach (PolygonCollider2D poly in polys) {
 			foreach (Vector2 point in poly.points) {
-				//allPoints.Add (point + (Vector2)poly.transform.position + poly.offset);
-				angles.Add (GetAngle (point) - 0.1f);
-				angles.Add (GetAngle (point));
-				angles.Add (GetAngle (point) + 0.1f);
+				Vector2 addPoint = point + (Vector2)poly.transform.position + poly.offset;
+				allPoints.Add(addPoint + new Vector2(Mathf.Sin(0.5f* GetAngle(addPoint)*Mathf.Deg2Rad), Mathf.Cos(0.5f* GetAngle(addPoint)*Mathf.Deg2Rad))*accuracyDegree);
+				allPoints.Add(addPoint + new Vector2(-Mathf.Sin(0.5f* GetAngle(addPoint)*Mathf.Deg2Rad), -Mathf.Cos(0.5f* GetAngle(addPoint)*Mathf.Deg2Rad))*accuracyDegree);
+				allPoints.Add (addPoint);
 			}
 		}
 		CastRays ();
+
+		DrawMesh ();
 	}
 
 	void CastRays() {
-		//first sort points based on their angle made with the x positive axis.
-		//allPoints.Sort(SortByAngle);
-		angles.Sort();
+
+		//second sort points based on their angle made with the x positive axis.
+		allPoints.Sort(SortByAngle);
 
 		//now raycast to each point and see what gwans
-		/*
 		foreach (Vector2 point in allPoints) {
 			Ray2D ray = new Ray2D (transform.position, (point - (Vector2)transform.position).normalized);
 			Debug.DrawRay (transform.position, ray.direction * viewRadius);
 			if(Physics2D.Raycast(ray.origin, ray.direction, viewRadius)) {
 				hitPoints.Add (Physics2D.Raycast (ray.origin, ray.direction, viewRadius).point);
 			} else {
-				hitPoints.Add (ray * viewRadius);
+				hitPoints.Add ((Vector2)transform.position + ray.direction * viewRadius);
 			}
 		}
-		*/
+	}
 
-		foreach (float angle in angles) {
-			Ray2D ray = new Ray2D (transform.position, GetDirFromAngle (angle));
-			Debug.DrawRay (transform.position, ray.direction * viewRadius);
+	void DrawMesh() {
+		int vertexCount = hitPoints.Count + 1;
+		Vector3[] vertices = new Vector3[vertexCount];
+		int[] triangles = new int[(vertexCount - 1) * 3];
+
+		vertices [0] = Vector3.zero;
+		for (int i = 0; i < vertexCount - 1; i++) {
+			vertices [i + 1] = transform.InverseTransformPoint (hitPoints [i]);
+			//vertices[i + 1] = hitPoints[i];
+
+			if (i < vertexCount - 2) {
+				triangles [i * 3] = 0;
+				triangles [i * 3 + 1] = i + 1;
+				triangles [i * 3 + 2] = i + 2;
+			}
 		}
 
+		triangles [(vertexCount - 1) * 3 - 3] = 0;
+		triangles [(vertexCount - 1) * 3 - 2] = vertexCount-1;
+		triangles [(vertexCount - 1) * 3 - 1] = 1;
+
+		shadowMesh.Clear ();
+		shadowMesh.vertices = vertices;
+		shadowMesh.triangles = triangles;
+		shadowMesh.RecalculateNormals ();
 	}
 
 	int SortByAngle(Vector2 vectA, Vector2 vectB) {
@@ -109,7 +136,7 @@ public class ShadowRays : MonoBehaviour {
 	}
 
 	Vector2 GetDirFromAngle(float angle) {
-		return new Vector2 (Mathf.Sin (angle * Mathf.Deg2Rad), Mathf.Cos (angle * Mathf.Deg2Rad));
+		return new Vector2 (Mathf.Cos (angle * Mathf.Deg2Rad), Mathf.Sin (angle * Mathf.Deg2Rad));
 	}
 
 	PolygonCollider2D[] GetColliders() {
@@ -145,7 +172,14 @@ public class ShadowRays : MonoBehaviour {
 		foreach (Vector2 point in allPoints) {
 			//i = GetAngle (point - (Vector2)transform.position);
 			//Gizmos.DrawCube (point, Vector3.one * i * Mathf.Deg2Rad);
-			Gizmos.DrawCube (point, Vector3.one/4);
+			Gizmos.DrawCube (point, Vector3.one/15);
+		}
+
+		foreach (Vector2 point in hitPoints) {
+			//i = GetAngle (point - (Vector2)transform.position);
+			//Gizmos.DrawCube (point, Vector3.one * i * Mathf.Deg2Rad);
+			Gizmos.color = Color.red;
+			Gizmos.DrawCube (point, Vector3.one/10);
 		}
 	}
 }
